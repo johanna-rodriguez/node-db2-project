3926552de8fb5d17c153cdc6ecb6677c3111d917
Johanna Rodriguez <>
2021-09-08 01:42:25 -0500

    complete server components

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,27 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  
+  return db('cars')
 }
 
-const getById = () => {
-  
+const getById = (id) => {
+    return db('cars').where('id', id).first()
 }
 
-const create = () => {
-  
+const getByVin = (vin) => {
+    return db('cars').where('vin', vin).first()
+}
+
+const create = (car) => {
+    return db('cars').insert(car)
+    .then(([id]) => {
+     return getById(id)
+    })
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    getByVin,
 }
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,7 +1,7 @@
 // STRETCH
 const cars = [
     {
-        vin: '57566876999',
+        vin: '1G1PG5SC9C7191727',
         make: 'toyota',
         model: 'rav4',
         mileage: 250000,
@@ -9,14 +9,14 @@ const cars = [
         transmission: 'Automatic',
     },
     {
-        vin: '65757869878',
+        vin: '1FUYDSEBXSP556994',
         make: 'toyota',
         model: '4Runner',
         mileage: 50000,
         title: 'salvage',
     },
     {
-        vin: '984924874264',
+        vin: 'JNKCV54E64M808593',
         make: 'jeep',
         model: 'grand cherokee',
         mileage: 5000,
